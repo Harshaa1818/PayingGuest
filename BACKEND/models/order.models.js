7623cb332e668cd08abc725cb4b986991609d5d0
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-08-12 23:48:59 +0530

    schema design

---
+++ BACKEND/models/order.models.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const orderSchema=new mongoose.Schema({
+    visitor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        },
+        property: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Property',
+        required: true,
+        },
+        startDate: {
+        type: Date,
+        required: true,
+        },
+        endDate: {
+        type: Date,
+        required: true,
+        },
+        status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'cancelled'],
+        default: 'pending',
+        },
+
+},
+{
+    timestamps:true
+})
+
+export const Order=mongoose.model("Order",orderSchema)
