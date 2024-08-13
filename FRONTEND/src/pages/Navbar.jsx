d7811a98932c840b15b6d9da53eee8d3a5fb5cfa
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-08-14 00:30:31 +0530

    tailwind config

--- FRONTEND/src/pages/Navbar.jsx
+++ FRONTEND/src/pages/Navbar.jsx
@@ -7,7 +7,7 @@ return (
     <>
     <div className="flex justify-around m-5 border-2">
         <div>
-            <h1>PG booking App</h1>
+            <h1 className="text-2xl">PG booking App</h1>
         </div>
 
         <div className="">
@@ -32,10 +32,10 @@ return (
         <div>
             <h1 className="flex justify-center p-5"> Our Services</h1>
             <div className="flex justify-around border-2">
-                <div style={{ position: "relative" }}></div>
+                <div className="relative"></div>
                     <img src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?cs=srgb&dl=pexels-pixabay-258154.jpg&fm=jpg" className="w-full" style={{ height: "450px", filter: "blur(2px)" }} alt="Service Image" />
-                    <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
-                        <h2 style={{ color: "white", fontSize: "24px", textAlign: "center" }}>Manage all Pg and hotels in your pocket!!!</h2>
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <h1 className="text-white text-center" style={{ fontSize: "24px" }} >Manage all Pg and hotels in your pocket!!!</h1>
                     </div>
                 </div>
             
