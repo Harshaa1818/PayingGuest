51db84721e7baa7ccb8f77680f4fd3722891e2d0
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-10-09 23:48:22 +0530

    prettier formatting

--- BACKEND/controllers/auth.controller.js
+++ BACKEND/controllers/auth.controller.js
@@ -1,73 +1,98 @@
+import {
+  signUpValidationSchema,
+  signInValidationSchema,
+} from "../validators/auth.validator.js";
+import { authService } from "../services/auth.service.js";
+import Crypto from "crypto";
 
-import { signUpValidationSchema, signInValidationSchema } from '../validators/auth.validator.js'
-import { authService } from '../services/auth.service.js'
-import Crypto from 'crypto'
+const handleSignIn = async (req, res) => {
+  try {
+    const validation = await signInValidationSchema.safeParseAsync(req.body);
 
-const handleSignIn = async( req,res ) => {
-    try{
-        const validation = await signInValidationSchema.safeParseAsync(req.body)
+    if (!validation.success)
+      return res
+        .status(400)
+        .json({
+          message: "error at validation",
+          error: validation.error.message,
+        });
 
-        if(!validation.success) return res.status(400).json({message: "error at validation", error: validation.error.message})
-         
-         const { email, password } = req.body;
-         
-         const user = await authService.findbyMailId(email)
-         
+    const { email, password } = req.body;
 
-         if(!user) return res.status(404).json({message: "user not found"})
-          
-            const salt = user.salt
+    const user = await authService.findbyMailId(email);
 
-            const hashedPassword =Crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
+    if (!user) return res.status(404).json({ message: "user not found" });
 
-            if(hashedPassword !== user.password) return res.status(404).json({message: "invalid password"})
-            const token = await authService.generateToken(user._id)
-            
+    const salt = user.salt;
 
-            return res.status(200).json({message: "login successfull", token:token, user })
-                
+    const hashedPassword = Crypto.pbkdf2Sync(
+      password,
+      salt,
+      1000,
+      64,
+      "sha512"
+    ).toString("hex");
 
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-}
-const handleSignUp = async( req,res ) => {
-    try{
-        const validation = await signUpValidationSchema.safeParseAsync(req.body)
-        
-       // if(validation.error) return res.status(400).json({message: `error is : ${validation.error.message}`})
+    if (hashedPassword !== user.password)
+      return res.status(404).json({ message: "invalid password" });
+    const token = await authService.generateToken(user._id);
 
-            const { email, username, password, contactNo } = validation.data;
+    return res
+      .status(200)
+      .json({ message: "login successfull", token: token, user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const handleSignUp = async (req, res) => {
+  try {
+    const validation = await signUpValidationSchema.safeParseAsync(req.body);
 
-            const user = await authService.findbyMailId(email)
-            if(user) return res.status(400).json({message: "user already exists"})
+    if (validation.error)
+      return res
+        .status(400)
+        .json({ message: `error is : ${validation.error.message}` });
 
-                const salt = Crypto.randomBytes(16).toString('hex')
-                const hashedPassword = Crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
-                
+    const { email, username, password, contactNo } = validation.data;
 
-                const newUser = await authService.register({ email, username, password:hashedPassword, role:"visitor", contactNo, salt })
+    const user = await authService.findbyMailId(email);
+    if (user) return res.status(400).json({ message: "user already exists" });
 
-                return res.json(201).json({ message:"user created succesfully", newUser })
-    }
-    catch(error){
-       return res.status(500).json({message: error.message})
-    }
-}
-const handleSignOut = async( req,res ) => {
-    try{
-        const id = req.id
-    
-        const user = await authService.findById(id)
+    const salt = Crypto.randomBytes(16).toString("hex");
+    const hashedPassword = Crypto.pbkdf2Sync(
+      password,
+      salt,
+      1000,
+      64,
+      "sha512"
+    ).toString("hex");
 
-        if(!user) return res.status(404).json({message: "user not found"})
+    const newUser = await authService.register({
+      email,
+      username,
+      password: hashedPassword,
+      role: "visitor",
+      contactNo,
+      salt,
+    });
 
-        res.status(200).json({message: "logout successfull", user }) 
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-}
+    return res.json(201).json({ message: "user created succesfully", newUser });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+const handleSignOut = async (req, res) => {
+  try {
+    const id = req.id;
 
-export { handleSignIn, handleSignUp, handleSignOut }
+    const user = await authService.findById(id);
+
+    if (!user) return res.status(404).json({ message: "user not found" });
+
+    res.status(200).json({ message: "logout successfull", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { handleSignIn, handleSignUp, handleSignOut };
